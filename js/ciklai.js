e4fab7287d5f0dab8bd43b35da60c8f29c08891f
Rimantas <belauzas@yahoo.com>
2021-01-08 19:28:16 +0200

    Ciklai

---
+++ js/ciklai.js
@@ -0,0 +1,27 @@
+/*
+Suskaiciuok nuo 1 iki 5.
+Atvaizdavimas:
+console.log(skaicius);
+*/
+
+const menesiai = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Geguze', 'Birzelis'];
+
+// console.log(menesiai[0]);
+// console.log(menesiai[1]);
+// console.log(menesiai[2]);
+// console.log(menesiai[3]);
+
+for (let i = 0; i < menesiai.length; i++) {
+    console.log(menesiai[i]);
+}
+
+
+
+const pazymiai = [10, 2, 8, 4, 6, 4, 8, 2, 10];     // 6
+
+let vidurkis = 0;
+
+// logika
+
+
+console.log('Pazymiu vidurkis:', vidurkis);
