43e920b88131d443ba8a2f52cbcd3fae90190b1c
Rimantas <belauzas@yahoo.com>
2021-01-13 20:41:31 +0200

    Letters from string

---
+++ js/letters.js
@@ -0,0 +1,11 @@
+function letters() {
+
+}
+
+console.log(letters('abcdefg', 2), '->', 'bdf');
+console.log(letters('abcdefghijkl', 3), '->', 'cfil');
+console.log(letters('abc', 0));
+console.log(letters('abc', 4));
+console.log(letters('abc', 1), '->', 'abc');
+console.log(letters('abcdef', 1), '->', 'abcdef');
+console.log(letters(1561, 2));
